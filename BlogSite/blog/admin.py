e19cee7d1029f4165c520eeb6a4daac5910da8b2
envs <olaonipekun2001@yahoo.com>
2020-03-27 14:02:18 +0200

    Field Updates

--- BlogSite/blog/admin.py
+++ BlogSite/blog/admin.py
@@ -9,7 +9,8 @@ class PostAdmin(admin.ModelAdmin):
 
     list_display = ('title', 'slug', 'author', 'publish', 'status')
     list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
     prepopulated_fileds = {'slug': ('title',)}
     raw_id_fields = ('author',)
     date_hierarchy = 'publish'
-    ordering
+    ordering = ("status", "publish")
