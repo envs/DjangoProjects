f24845a761502e20fac0a9dcfc17a79838c493da
envs <olaonipekun2001@yahoo.com>
2020-03-27 14:46:35 +0200

    Field Updates

--- BlogSite/blog/admin.py
+++ BlogSite/blog/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Product
+from .models import Post
 
 # Register your models here.
-admin.site.register(Product)
+#admin.site.register(Post)
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
