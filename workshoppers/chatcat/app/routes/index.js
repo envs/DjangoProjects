37a562f21e5ad413997967ed5ede254d8229acf8
envs <olaonipekun2001@yahoo.com>
2020-04-01 23:14:24 +0200

    Updates

---
+++ workshoppers/chatcat/app/routes/index.js
@@ -0,0 +1,45 @@
+'use strict';
+const router = require('express').Router();
+
+module.exports = () => {
+    let routes = {
+        'get': {
+            '/': (req, res, next) => {
+                //res.send('<h1>Hello Express</h1>');
+                //res.sendFile(__dirname + '/views/login.htm');
+                res.render('login', {
+                    pageTitle: "My Login Page"
+                });
+            },
+            '/rooms': (req, res, next) => {
+                res.render('rooms');
+            },
+            '/chat': (req, res, next) => {
+                res.render('chatroom')
+            }
+        },
+        'post': {
+
+        }
+    }
+
+    // Iterate through the routes objects  and mount the routes
+    let registerRoutes = (routes, method) => {
+        for(let key in routes) {
+            if(typeof routes[key] === 'object' && routes[key] !== null && !(routes[key] instanceof Array)) {
+                registerRoutes(routes[key], key);
+            } else {
+                // Register the routes
+                if (method === 'get') {
+                    router.get(key, routes[key]);
+                } else if (method === 'post') {
+                    router.post(key, routes[key]);
+                }
+            }
+        }
+    }
+
+    registerRoutes(routes);
+
+    return router;
+}
--- workshoppers/chatcat/server.js
+++ workshoppers/chatcat/server.js
@@ -4,17 +4,13 @@ const express = require('express');
 
 // Instantiate the app object and set properties
 const app = express();
+const chatCat = require('./app');
+
 app.set('port', process.env.PORT || 3000);
+app.use(express.static('public'));          // Serve static files
 app.set('view engine', 'ejs');
 
-// Basic Routes
-app.get('/', (req, res, next) => {
-    //res.send('<h1>Hello Express</h1>');
-    //res.sendFile(__dirname + '/views/login.htm');
-    res.render('login', {
-        pageTitle: "My Login Page"
-    });
-});
+app.use('/', chatCat.router);
 
 // App listening on Port and Console output
 app.listen(app.get('port'), ()=>{
