925b137577cb84bf172321b28d584ff7cc83a245
envs <olaonipekun2001@yahoo.com>
2020-05-05 12:42:38 +0200

    ML Classifier with RandomForest

--- ImpracticalPython/MLClassifier.py
+++ ImpracticalPython/MLClassifier.py
@@ -3,6 +3,8 @@ import pandas as pd
 import re
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import KFold, cross_val_score, train_test_split
+from sklearn.metrics import precision_recall_fscore_support as score
 import string
 
 stopwords = nltk.corpus.stopwords.words('english')
@@ -32,3 +34,22 @@ X_features = pd.concat([data['body_len'], data['punct%'], pd.DataFrame(X_tfidf.t
 X_features.head()
 
 # Explore RandomForestClassifier Attributes and Hyperparameters
+print(dir(RandomForestClassifier))
+print(RandomForestClassifier())
+
+# Explore RandomForestClassifier through Cross-Validation
+rf = RandomForestClassifier(n_jobs=-1)  # Setting the -1 value enables the process to be faster by building the individual decision trees in parallel
+k_fold = KFold(n_splits=5)  # KFold assigns each observation to a certain subset. So for n_splits=5, it will train on 4 subset and evaluate on 1
+cross_val_score(rf, X_features, data['label'], cv=k_fold, scoring='accuracy', n_jobs=-1)
+
+# Explore RandomForestCLassifier through Holdout Set
+X_train, X_test, y_train, y_test = train_test_split(X_features, data['label'], test_size=0.2)
+rf = RandomForestClassifier(n_estimators=50, max_depth=20, n_jobs=-1)
+rf_model = rf.fit(X_train, y_train)
+
+sorted(zip(rf_model.feature_importances_, X_train.columns), reverse=True)[:10]
+
+y_pred = rf_model.predict(X_test)
+precision, recall, fscore, support = score(y_test, y_pred, pos_label='spam', average='binary')
+
+print('Precision: {} / Recall: {} / Accuracy: {}'.format(round(precision, 3), round(recall, 3), round((y_pred==y_test).sum() / len(y_pred), 3)))
